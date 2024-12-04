9d26e3e22f8948ba78ef7c2ac43b04cbc115d570
William Gregorio <mail@williamgregorio.com>
2024-12-03 23:41:46 -0500

    added last piece on cp syst from polic

--- bin/blrplate.js
+++ bin/blrplate.js
@@ -4,6 +4,7 @@ const { Command } = require('commander');
 const path = require('path');
 const { createThemeDirectories } = require('../lib/createDirectories.js');
 const { initThemeConfig } = require('../lib/initThemeConfig.js');
+const { copyFilesFromData } = require('../lib/fileManagement.js'); 
 
 const program = new Command();
 
@@ -13,6 +14,7 @@ program
   .action(async () => {
     const currentDir = process.cwd();
 
+    console.log('Creating theme directories...');
     try {
       await createThemeDirectories(currentDir);
     } catch (error) {
@@ -22,11 +24,21 @@ program
 
     const configDir = path.join(currentDir, 'config');
 
+    console.log('Initializing theme configuration...');
     try {
       await initThemeConfig(configDir);
     } catch (error) {
       console.error(`Error creating theme config: ${error.message}`);
     }
+
+    try {
+      await copyFilesFromData(currentDir);
+    } catch (error) {
+      console.error(`Error copying files: ${error.message}`);
+      return;
+    }
+
+    console.log('Your Shopify theme boilerplate is ready to be developed.');
   });
 
 program.parse(process.argv);
