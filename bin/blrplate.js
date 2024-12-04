c5fa12657c009dc1dee0fcf1276665b18855fb71
William Gregorio <mail@williamgregorio.com>
2024-12-03 20:16:57 -0500

    new test strc quick

--- bin/blrplate.js
+++ bin/blrplate.js
@@ -2,7 +2,31 @@
 
 const { Command } = require('commander');
 const path = require('path');
+const { createThemeDirectories } = require('../createDirectories');
+const { initThemeConfig } = require('../initThemeConfig');
 
 const program = new Command();
-console.log(program);
 
+program
+  .command('create')
+  .description('Create a new Shopify theme boilerplate')
+  .action(async () => {
+    const currentDir = process.cwd();
+
+    try {
+      await createThemeDirectories(currentDir);
+    } catch (error) {
+      console.error(`Error creating directories: ${error.message}`);
+      return;
+    }
+
+    const configDir = path.join(currentDir, 'config');
+
+    try {
+      await initThemeConfig(configDir);
+    } catch (error) {
+      console.error(`Error creating theme config: ${error.message}`);
+    }
+  });
+
+program.parse(process.argv);
